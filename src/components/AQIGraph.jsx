fb6dfdf9f1fffd3d609c0b8fbdfefc0fb0ffc827
cumaniar <chiragma18633@gmail.com>
2021-05-16 02:52:06 +0530

    Command change

--- src/components/AQIGraph.jsx
+++ src/components/AQIGraph.jsx
@@ -83,6 +83,7 @@ const AQIGraph = ({ cityData }) => {
         </p>
         <Line ref={chartRef} data={{ datasets: [] }} options={chartJsConfig} />
       </div>
+      <img src='./aqi-info.png' alt='aqi-info' height="400" />
     </div>
   );
 };
